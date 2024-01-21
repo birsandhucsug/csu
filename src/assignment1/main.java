8380b36c6618c4336d4dae7974f56eadddafce40
Bir Sandhu <bir.sandhu@csuglobal.edu>
2024-01-21 15:55:22 +0100

    Initial commit

---
+++ src/assignment1/main.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Restaurant name: Byte sized");
+//drowning in originality with this one
+System.out.println("Business address: 01100011 01110011 01110101 Cyber Street");
+System.out.println("City, State, Zip code: Colorado Springs, Colorado, 80901");
+	}
+
+}
